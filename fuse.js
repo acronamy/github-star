f37d8f5548dad9fdf5f416797c49fe2d4aec304c
Adam crockett <adam.crockett@dyson.com>
2019-06-22 22:46:03 +0100

    started work on building react components

---
+++ fuse.js
@@ -0,0 +1,22 @@
+const { FuseBox, SassPlugin, CSSPlugin, HTMLPlugin, BabelPlugin } = require("fuse-box");
+
+const fuse = FuseBox.init({
+  homeDir: "src",
+  output: "build/$name.js",
+  target: "browser@es6",
+  plugins: [
+    BabelPlugin({
+      config: {
+        sourceMaps: true,
+        presets: ["@babel/preset-react"],
+        plugins: [["transform-react-jsx"]],
+      },
+    }),
+    SassPlugin(),
+    CSSPlugin()
+  ]
+});
+
+fuse.bundle("github-star").instructions(`> index.ts`);
+
+fuse.run();
