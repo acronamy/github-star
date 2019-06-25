dadfc0f9d1c22b9bcc4d5596b272bbf0c858bfaa
Adam crockett <adam.crockett@dyson.com>
2019-06-25 20:23:01 +0100

    created react base app which all other components are used

--- packages/app/app.jsx
+++ packages/app/app.jsx
@@ -1,17 +1,39 @@
-import { Component } from "react";
-import { render } from "react-dom";
-import "./app.scss";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as appStyle from "./app.scss";
+import {previousRelativeMonthDay} from "../app-server/utils/time-utils";
+import { ComponentTitle } from "../component-title/component-title";
+import { ComponentList } from "../component-list/component-list";
+import { ComponentDate } from "../component-date/component-date";
 
-class App extends Component {
+class App extends React.Component {
+  static getMonthStartRange () {
+    const now = new Date();
+    const [year, month, day] = [now.getFullYear(), now.getMonth(), now.getDate()];
+    const lastMonthsDay = previousRelativeMonthDay({
+      year,
+      month,
+      day
+    });
+    return [year, month - 1, lastMonthsDay]
+  }
   render() {
     return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+      <div className="str">
+        <div className='str-Panel'>
+          {this.props.children}
+        </div>
+        <style>{appStyle}</style>
       </div>
     );
   }
 }
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+ReactDOM.render(
+  <App>
+    <ComponentTitle>
+      <ComponentDate prefix='Repos created since' date={App.getMonthStartRange()}/>
+    </ComponentTitle>
+    <ComponentList/>
+  </App>,
+document.getElementById("root"));
